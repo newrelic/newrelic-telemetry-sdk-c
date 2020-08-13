2f4c9ffe3719546c8e89e632c1b982c784cc38af
Johannes Tax <jtax@newrelic.com>
2020-08-13 15:37:11 -0700

    Define an API for the Telemetry SDK (#3)

--- include/newrelic-telemetry-sdk.h
+++ include/newrelic-telemetry-sdk.h
@@ -1,30 +1,273 @@
 #ifndef NEWRELIC_TELEMETRY_SDK
 #define NEWRELIC_TELEMETRY_SDK
 
+#include <stdbool.h>
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/**
+ * A Sender is capable of both queuing and sending span and metrics batches to
+ * a configured New Relic collector.
+ */
 typedef struct _nrt_sender_t nrt_sender_t;
+
+/**
+ * A span batch
+ *
+ * A collection of spans.
+ */
+typedef struct _nrt_span_batch_t nrt_span_batch_t;
+
+/**
+ * A span
+ */
+typedef struct _nrt_span_t nrt_span_t;
+
+/**
+ * A collection of attributes
+ */
+typedef struct _nrt_attributes_t nrt_attributes_t;
+
+/**
+ * Indicates a point in time or a duration.
+ *
+ * A point in time is indicated by a time in Epoch milliseconds, a duration is
+ * indicated by milliseconds.
+ */
+typedef uint64_t nrt_time_t;
+
+/**
+ * Create a new attribute collection.
+ *
+ * Allocate and initialize an empty attribute collection
+ *
+ * @return an empty attribute collection
+ */
+nrt_attributes_t* nrt_attributes_new();
+
+/**
+ * Add an int attribute to an attribute collection
+ *
+ * @param attributes
+ * @param key the attribute key
+ * @param value the attribute value
+ * @return true if the attribute was added
+ */
+bool nrt_attributes_set_int(nrt_attributes_t* attributes,
+                            const char* key,
+                            int64_t value);
+
+/**
+ * Add an unsigned int attribute to an attribute collection
+ *
+ * @param attributes
+ * @param key the attribute key
+ * @param value the attribute value
+ * @return true if the attribute was added
+ */
+bool nrt_attributes_set_uint(nrt_attributes_t* attributes,
+                             const char* key,
+                             uint64_t value);
+
+/**
+ * Add a double attribute to an attribute collection
+ *
+ * @param attributes
+ * @param key the attribute key
+ * @param value the attribute value
+ * @return true if the attribute was added
+ */
+bool nrt_attributes_set_double(nrt_attributes_t* attributes,
+                               const char* key,
+                               double value);
+
+/**
+ * Add a string attribute to an attribute collection
+ *
+ * @param attributes
+ * @param key the attribute key
+ * @param value the attribute value
+ * @return true if the attribute was added
+ */
+bool nrt_attributes_set_string(nrt_attributes_t* attributes,
+                               const char* key,
+                               const char* value);
+
+/**
+ * Add a bool attribute to an attribute collection
+ *
+ * @param attributes
+ * @param key the attribute key
+ * @param value the attribute value
+ * @return true if the attribute was added
+ */
+bool nrt_attributes_set_bool(nrt_attributes_t* attributes,
+                             const char* key,
+                             bool value);
+
+/**
+ * Destroy an attribute collection
+ *
+ * Destroy the attribute collection without adding it to a span. The passed
+ * pointer will be set to NULL.
+ *
+ * @param attributes an attribute collection
+ */
+void nrt_attributes_destroy(nrt_attributes_t** attributes);
+
+/**
+ * Create a new span.
+ *
+ * Allocate and initialize a new span.
+ *
+ * @param id the span id
+ * @param trace_id the trace id
+ * @param parent_id the parent id. Passing NULL indicates that the span has no
+ * parent.
+ * @return a span
+ */
+nrt_span_t* nrt_span_new(const char* id,
+                         const char* trace_id,
+                         const char* parent_id);
+
+/**
+ * Set the name of a span
+ *
+ * @param span
+ * @param name the name for the span
+ * @return true if the name could be set
+ */
+bool nrt_span_set_name(nrt_span_t* span, const char* name);
+
+/**
+ * Set the service name of a span
+ *
+ * @param span
+ * @param service_name the service name for the span
+ * @return true if the service name could be set
+ */
+bool nrt_span_set_service_name(nrt_span_t* span, const char* service_name);
+
+/**
+ * Set the start timestamp for a span
+ *
+ * @param span
+ * @param timestamp the start timestamp for the span in Epoch milliseconds
+ * @return true if the start timestamp could be set
+ */
+bool nrt_span_set_timestamp(nrt_span_t* span, nrt_time_t timestamp);
+
+/**
+ * Set the duration for a span
+ *
+ * @param span
+ * @param duration the duration for the span in milliseconds
+ * @return true if the duration could be set
+ */
+bool nrt_span_set_duration(nrt_span_t* span, nrt_time_t duration);
+
+/**
+ * Set attributes on a span
+ *
+ * If the attributes were successfully added to the span, the span takes
+ * ownership of the attribute collection. Otherwise the attribute collection
+ * will be destroyed. The passed pointer to the attribute collection will
+ * always be set to NULL.
+ *
+ * @param span
+ * @param attributes a collection of attributes
+ */
+bool nrt_span_set_attributes(nrt_span_t* span, nrt_attributes_t** attributes);
+
+/**
+ * Destroy a span.
+ *
+ * Destroy a span without adding it to a batch. The passed pointer will be set
+ * to NULL.
+ *
+ * @param span
+ */
+void nrt_span_destroy(nrt_span_t** span);
+
+/**
+ * Create a new span batch.
+ *
+ * A span batch is a collection of spans that is sent in one payload.
+ *
+ * @return an empty span batch
+ */
+nrt_span_batch_t* nrt_span_batch_new();
+
+/**
+ * Add a span to a span batch.
+ *
+ * If the span was successfully added to the batch, the batch takes ownership
+ * of the span. Otherwise the span will be destroyed. The passed pointer will
+ * always be set to NULL.
+ *
+ * @param batch a span batch
+ * @param span a span
+ * @return true if the span was successfully added to the batch
+ */
+bool nrt_span_batch_record(nrt_span_batch_t* batch, nrt_span_t** span);
+
+/**
+ * Destroy a span batch.
+ *
+ * Spans previously added to the span batch will be lost. The passed pointer
+ * will be set to NULL.
+ *
+ * @param batch a span batch
+ */
+void nrt_span_batch_destroy(nrt_span_batch_t** batch);
 
 /**
  * Create a new sender.
  *
- * Senders can be used to send span and metrics batches.
- *
  * @param key an Insights API key
  * @return a sender
  */
-nrt_sender_t *nrt_sender_new(const char *key);
+nrt_sender_t* nrt_sender_new(const char* key);
+
+/**
+ * Send a span batch.
+ *
+ * Put a span batch in the queue of the sender. This function returns as soon
+ * as the span batch was queued and doesn't wait for it to be sent
+ * successfully.
+ *
+ * If the span batch is successfully queued, the sender takes ownership of the
+ * span batch. Otherwise the batch will be destroyed. The passed pointer will
+ * always be set to NULL.
+ *
+ * @param sender a sender
+ * @param sender the span batch to be sent
+ * @return true if the span batch was successfully queued
+ */
+bool nrt_sender_send(nrt_sender_t* sender, nrt_span_batch_t** batch);
 
 /**
  * Shutdown a sender.
  *
- * Shuts down the sender, sends pending data and frees the sender object.
+ * Shuts down the sender, sends pending data and frees the sender object. The
+ * passed pointer will be set to NULL.
  *
  * @param sender a sender
  */
-void nrt_sender_shutdown(nrt_sender_t *sender);
+void nrt_sender_shutdown(nrt_sender_t** sender);
+
+/**
+ * Destroy a sender.
+ *
+ * Destroy the sender, without making sure pending data is sent. The passed
+ * pointer will be set to NULL.
+ *
+ * @param sender a sender
+ */
+void nrt_sender_destroy(nrt_sender_t** sender);
 
 #ifdef __cplusplus
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,128 @@ use std::os::raw::c_char;
 use std::ptr;
 
 pub struct Sender {}
+pub struct SpanBatch {}
+pub struct Span {}
+pub struct Attributes {}
 
 #[no_mangle]
-pub extern "C" fn nrt_sender_new(key: *const c_char) -> *mut Sender {
-    return ptr::null_mut();
+pub extern "C" fn nrt_attributes_new() -> *mut Attributes {
+    ptr::null_mut()
 }
 
 #[no_mangle]
-pub extern "C" fn nrt_sender_shutdown(sender: *mut Sender) {}
+pub extern "C" fn nrt_attributes_set_int(
+    attributes: *mut Attributes,
+    key: *const c_char,
+    value: i64,
+) -> bool {
+    false
+}
+
+#[no_mangle]
+pub extern "C" fn nrt_attributes_set_uint(
+    attributes: *mut Attributes,
+    key: *const c_char,
+    value: u64,
+) -> bool {
+    false
+}
+
+#[no_mangle]
+pub extern "C" fn nrt_attributes_set_double(
+    attributes: *mut Attributes,
+    key: *const c_char,
+    value: f64,
+) -> bool {
+    false
+}
+
+#[no_mangle]
+pub extern "C" fn nrt_attributes_set_string(
+    attributes: *mut Attributes,
+    key: *const c_char,
+    value: *const c_char,
+) -> bool {
+    false
+}
+
+#[no_mangle]
+pub extern "C" fn nrt_attributes_set_bool(
+    attributes: *mut Attributes,
+    key: *const c_char,
+    value: bool,
+) -> bool {
+    false
+}
+
+#[no_mangle]
+pub extern "C" fn nrt_attributes_destroy(span: *mut *mut Attributes) {}
+
+#[no_mangle]
+pub extern "C" fn nrt_span_new(
+    id: *const c_char,
+    trace_id: *const c_char,
+    parent_id: *const c_char,
+) -> *mut Span {
+    ptr::null_mut()
+}
+
+#[no_mangle]
+pub extern "C" fn nrt_span_set_name(span: *mut Span, name: *const c_char) -> bool {
+    false
+}
+
+#[no_mangle]
+pub extern "C" fn nrt_span_set_service_name(span: *mut Span, service_name: *const c_char) -> bool {
+    false
+}
+
+#[no_mangle]
+pub extern "C" fn nrt_span_set_timestamp(span: *mut Span, timestamp: u64) -> bool {
+    false
+}
+
+#[no_mangle]
+pub extern "C" fn nrt_span_set_duration(span: *mut Span, duration: u64) -> bool {
+    false
+}
+
+#[no_mangle]
+pub extern "C" fn nrt_span_set_attributes(
+    span: *mut Span,
+    attributes: *mut *mut Attributes,
+) -> bool {
+    false
+}
+
+#[no_mangle]
+pub extern "C" fn nrt_span_destroy(span: *mut *mut Span) {}
+
+#[no_mangle]
+pub extern "C" fn nrt_span_batch_new() -> *mut SpanBatch {
+    ptr::null_mut()
+}
+
+#[no_mangle]
+pub extern "C" fn nrt_span_batch_record(batch: *mut SpanBatch, span: *mut *mut Span) -> bool {
+    true
+}
+
+#[no_mangle]
+pub extern "C" fn nrt_span_batch_destroy(batch: *mut *mut SpanBatch) {}
+
+#[no_mangle]
+pub extern "C" fn nrt_sender_new(key: *const c_char) -> *mut Sender {
+    ptr::null_mut()
+}
+
+#[no_mangle]
+pub extern "C" fn nrt_sender_send(key: *const c_char, batch: *mut *mut SpanBatch) -> bool {
+    false
+}
+
+#[no_mangle]
+pub extern "C" fn nrt_sender_shutdown(sender: *mut *mut Sender) {}
+
+#[no_mangle]
+pub extern "C" fn nrt_sender_destroy(sender: *mut *mut Sender) {}
