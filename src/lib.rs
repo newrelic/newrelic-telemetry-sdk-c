933a10a99db87c7cc257545c5daa828c029b38aa
Johannes Tax <jtax@newrelic.com>
2020-08-14 11:55:15 -0700

    Implement nrt_attribute_* functions, add submodule

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,37 @@
+use newrelic_telemetry::attribute::{ToValue, Value};
+use std::collections::HashMap;
+use std::ffi::CStr;
 use std::os::raw::c_char;
 use std::ptr;
 
 pub struct Sender {}
 pub struct SpanBatch {}
 pub struct Span {}
-pub struct Attributes {}
+type Attributes = HashMap<String, Value>;
 
 #[no_mangle]
 pub extern "C" fn nrt_attributes_new() -> *mut Attributes {
-    ptr::null_mut()
+    let attrs: HashMap<String, Value> = HashMap::new();
+    Box::into_raw(Box::new(attrs))
+}
+
+fn nrt_attributes_set<T: ToValue>(
+    attributes: *mut Attributes,
+    key: *const c_char,
+    value: T,
+) -> bool {
+    if attributes.is_null() || key.is_null() {
+        return false;
+    }
+
+    if let Some(attrs) = unsafe { attributes.as_mut() } {
+        if let Ok(key) = unsafe { CStr::from_ptr(key).to_str() } {
+            attrs.insert(key.to_string(), value.to_attribute_value());
+            return true;
+        }
+    }
+
+    false
 }
 
 #[no_mangle]
@@ -17,7 +40,7 @@ pub extern "C" fn nrt_attributes_set_int(
     key: *const c_char,
     value: i64,
 ) -> bool {
-    false
+    nrt_attributes_set(attributes, key, value)
 }
 
 #[no_mangle]
@@ -26,7 +49,7 @@ pub extern "C" fn nrt_attributes_set_uint(
     key: *const c_char,
     value: u64,
 ) -> bool {
-    false
+    nrt_attributes_set(attributes, key, value)
 }
 
 #[no_mangle]
@@ -35,7 +58,7 @@ pub extern "C" fn nrt_attributes_set_double(
     key: *const c_char,
     value: f64,
 ) -> bool {
-    false
+    nrt_attributes_set(attributes, key, value)
 }
 
 #[no_mangle]
@@ -44,7 +67,15 @@ pub extern "C" fn nrt_attributes_set_string(
     key: *const c_char,
     value: *const c_char,
 ) -> bool {
-    false
+    if value.is_null() {
+        return false;
+    }
+
+    if let Ok(value) = unsafe { CStr::from_ptr(value).to_str() } {
+        nrt_attributes_set(attributes, key, value)
+    } else {
+        false
+    }
 }
 
 #[no_mangle]
@@ -53,11 +84,16 @@ pub extern "C" fn nrt_attributes_set_bool(
     key: *const c_char,
     value: bool,
 ) -> bool {
-    false
+    nrt_attributes_set(attributes, key, value)
 }
 
 #[no_mangle]
-pub extern "C" fn nrt_attributes_destroy(span: *mut *mut Attributes) {}
+pub extern "C" fn nrt_attributes_destroy(attributes: *mut *mut Attributes) {
+    if attributes.is_null() {
+        return;
+    }
+    unsafe { Box::from_raw(*attributes) };
+}
 
 #[no_mangle]
 pub extern "C" fn nrt_span_new(
@@ -93,7 +129,14 @@ pub extern "C" fn nrt_span_set_attributes(
     span: *mut Span,
     attributes: *mut *mut Attributes,
 ) -> bool {
-    false
+    if attributes.is_null() {
+        false
+    } else if span.is_null() {
+        nrt_attributes_destroy(attributes);
+        return false;
+    } else {
+        false
+    }
 }
 
 #[no_mangle]
