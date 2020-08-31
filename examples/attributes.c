ea081d8accf9ed7f5337ccdfb7697e8e9e600f22
Johannes Tax <jtax@newrelic.com>
2020-08-31 13:11:46 -0700

    Add API

--- examples/attributes.c
+++ examples/attributes.c
@@ -1,5 +1,4 @@
 #include "newrelic-telemetry-sdk.h"
-#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,20 +12,10 @@ int main() {
 
   /* Initialize and destroy attributes of all types. */
   attrs = nrt_attributes_new();
-  assert(attrs);
   nrt_attributes_set_int(attrs, "int", -6);
   nrt_attributes_set_uint(attrs, "uint", 6);
   nrt_attributes_set_double(attrs, "double", 3.14159);
   nrt_attributes_set_string(attrs, "string", "value");
   nrt_attributes_set_bool(attrs, "bool", true);
   nrt_attributes_destroy(&attrs);
-  assert(NULL == attrs);
-
-  /* Call with NULL values */
-  nrt_attributes_set_int(NULL, NULL, -6);
-  nrt_attributes_set_uint(NULL, NULL, 6);
-  nrt_attributes_set_double(NULL, NULL, 3.14159);
-  nrt_attributes_set_string(NULL, NULL, NULL);
-  nrt_attributes_set_bool(NULL, NULL, true);
-  nrt_attributes_destroy(NULL);
 }
