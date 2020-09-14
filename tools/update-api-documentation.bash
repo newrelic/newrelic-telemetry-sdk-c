e79d7e7d961cb2539e1dd3523959ddc38580c2dc
Kyle Kneitinger <kyle@kneit.in>
2020-09-14 15:37:41 -0700

    Move doc generation job to its own workflow

--- tools/update-api-documentation.bash
+++ tools/update-api-documentation.bash
@@ -1,11 +1,10 @@
 #!/bin/bash
-#
 
 set -e
 
-if [ "$1" = "-h" -o "$1" = "--help" -o -z "$1" ] ; then
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     echo -e \
-	 "Usage: $0 <branchname>\n" \
+	 "Usage: $0\n" \
 	 "\n" \
          "This scripts creates doxygen API documentation based on the given\n" \
 	 "branch. It then creates or updates the branch\n\n" \
@@ -24,10 +23,15 @@ if [ ! -d ".git" ] ; then
     exit 1
 fi
 
+git fetch
+git config --global user.email "php-agent@newrelic.com"
+git config --global user.name "Doc Generator"
+
+# The base branch the documentation is built from
 branch="$1"
 
-logfile=$(basename $0).log
-exec > $logfile
+logfile=$(basename "$0").log
+exec > "$logfile"
 
 # The branch which will hold the final state that can be synced to the public
 # C Telemetry SDK repository.
@@ -39,18 +43,16 @@ release_branch="gh-pages"
 #
 do_cleanup() {
     rm -rf html/
-    git checkout $branch > /dev/null 2>&1
+    git checkout "$branch" > /dev/null 2>&1
 }
 trap do_cleanup EXIT
-do_cleanup
-
 
 # Step 1
 # ======
 #
 # Check out the base branch and run doxygen.
 #
-git checkout $branch
+git checkout "$branch"
 cd _doxygen
 doxygen
 cd ../
@@ -68,7 +70,6 @@ else
     git reset --hard origin/$release_branch
 fi
 
-
 # Step 3
 # ======
 # Remove all known files and replace them with the new doxygen output.
@@ -77,3 +78,4 @@ git rm -f $(git ls-files)
 git add $(find html)
 git mv html/* .
 git commit --allow-empty -m "Update documentation"
+git push origin "$release_branch"
