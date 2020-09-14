1edf863c80d8a8691ae1b0a86e435abefa650e9c
Fahmy Mohammed <fmohammed@newrelic.com>
2020-09-14 13:50:45 -0700

    remove docs directory

--- docs/search/enums_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['nrt_5flog_5flevel_5ft_79',['nrt_log_level_t',['../newrelic-telemetry-sdk_8h.html#abb203b927797ff3326ec75fdb3541751',1,'newrelic-telemetry-sdk.h']]]
-];
--- docs/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['newrelic_2dtelemetry_2dsdk_2eh_40',['newrelic-telemetry-sdk.h',['../newrelic-telemetry-sdk_8h.html',1,'']]]
-];
